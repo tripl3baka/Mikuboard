1b615315b14e53f038556d695d518428f043b76d
sonni <gombczysko@gmail.com>
2023-06-03 22:53:17 +0200

    Catalog

--- src/main/java/com/example/imageboard/controller/MainPageController.java
+++ src/main/java/com/example/imageboard/controller/MainPageController.java
@@ -6,19 +6,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import com.example.imageboard.model.Thread;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.io.IOException;
 import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
-
 
 @Controller
 public class MainPageController {
@@ -69,6 +65,12 @@ public class MainPageController {
 
         Resource file = fileStorageService.openFile(filename);
         return ResponseEntity.ok().body(file);
+    }
+
+    @GetMapping("/m/catalog")
+    public String catalog(Model model){
+        model.addAttribute("threads", threadRepository.findAll());
+        return "catalog";
     }
 
 }
