eb32c09af38ea042f8300353a185ff556cb329c9
sonni <gombczysko@gmail.com>
2023-06-21 13:40:58 +0200

    Admin login logic

--- src/main/java/com/example/imageboard/controller/AdminLoginController.java
+++ src/main/java/com/example/imageboard/controller/AdminLoginController.java
@@ -7,14 +7,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.function.Predicate;
+
 @Controller
 public class AdminLoginController {
     
     LoginRepository loginRepository;
+    private String hashedAdminPassword;
+
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-
     public AdminLoginController(LoginRepository loginRepository) {
         this.loginRepository = loginRepository;
     }
@@ -24,12 +27,21 @@ public class AdminLoginController {
 
     @PostMapping(value= "/m/login-post")
     public String adminLogin(AdminLoginData adminLoginData){
-        LoginCredentials loginCredentials = new LoginCredentials();
-        loginCredentials.setEncodedPassword(passwordEncoder.encode(adminLoginData.password));
-        loginRepository.saveAndFlush(loginCredentials);
+        LoginCredentials loginCredentials = loginRepository.findById(2).get();
+            hashedAdminPassword = loginCredentials.getEncodedPassword();
 
+//        loginCredentials.setEncodedPassword(passwordEncoder.encode(adminLoginData.password));
+//        loginRepository.saveAndFlush(loginCredentials);
+
+        if(compareHashedPasswordsResult(passwordEncoder.encode(adminLoginData.password),
+                e -> passwordEncoder.matches(adminLoginData.password, e))){
+            System.out.println("dupaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        }
         return "redirect:/m";
     }
 
+    private <T> boolean compareHashedPasswordsResult(T input, Predicate<T> predicate){
+        return predicate.test(input);
+    }
 
 }
